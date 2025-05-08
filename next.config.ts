7578223995926ab126db37de205bf74b6f63815f
Julian Thatguy <jborner111@gmail.com>
2025-05-08 18:03:39 -0500

    test build 2.6

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   images:{
     domains:['assets.aceternity.com']
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/homeRedux', // Replace with your desired default page path
+        permanent: true, // Or false for a temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
